40d9cade365061827a3cea5fdbf3e27afdf6fea0
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2023-07-08 00:52:42 +0100

    deterministic styles and fixes (#20)
    Closes #15

--- packages/dev/src/sheet/constants.ts
+++ packages/dev/src/sheet/constants.ts
@@ -1,46 +1,50 @@
 import type * as CSS from 'csstype';
-import type { Config } from '~/config';
+import type { Theme } from '~/theme';
 
-type PropertyDef = { themeKey: keyof Config['theme'] | 'unknown'; initialValue: string };
+type TokenamiProperty = keyof typeof PROPERTY_TO_TYPE & {};
+type PropertyConfig = { themeKey: keyof Theme | 'unknown'; initial?: string };
 
+const PROPERTY_TO_TYPE = {
+  color: { themeKey: 'colors' },
+  'background-color': { themeKey: 'colors' },
+  'padding-top': { themeKey: 'space', initial: '0' },
+  'padding-left': { themeKey: 'space', initial: '0' },
+  'padding-right': { themeKey: 'space', initial: '0' },
+  'padding-bottom': { themeKey: 'space', initial: '0' },
+  'margin-top': { themeKey: 'space', initial: '0' },
+  'margin-bottom': { themeKey: 'space', initial: '0' },
+  'margin-left': { themeKey: 'space', initial: '0' },
+  'margin-right': { themeKey: 'space', initial: '0' },
+  'column-gap': { themeKey: 'space', initial: '0' },
+  'row-gap': { themeKey: 'space', initial: '0' },
+  'border-radius': { themeKey: 'radii' },
+  width: { themeKey: 'unknown' },
+  'min-width': { themeKey: 'unknown' },
+  height: { themeKey: 'unknown' },
+  'min-height': { themeKey: 'unknown' },
+  display: { themeKey: 'unknown' },
+  'flex-direction': { themeKey: 'unknown' },
+  'align-items': { themeKey: 'unknown' },
+  'justify-content': { themeKey: 'unknown' },
+  'grid-template-columns': { themeKey: 'unknown' },
+  'grid-template-areas': { themeKey: 'unknown' },
+  'grid-template-rows': { themeKey: 'unknown' },
+  'font-size': { themeKey: 'unknown' },
+  'font-weight': { themeKey: 'unknown' },
+  'font-family': { themeKey: 'unknown' },
+  'line-height': { themeKey: 'unknown' },
+  border: { themeKey: 'unknown' },
+  'border-width': { themeKey: 'unknown' },
+  'border-style': { themeKey: 'unknown' },
+  'text-align': { themeKey: 'unknown' },
+  'object-fit': { themeKey: 'unknown' },
+  overflow: { themeKey: 'unknown' },
+} satisfies Partial<Record<keyof CSS.PropertiesHyphen, PropertyConfig>>;
+
+const ALL_PROPERTIES = Object.keys(PROPERTY_TO_TYPE) as (keyof typeof PROPERTY_TO_TYPE)[];
 const ALL_PSEUDO = ['hover', 'focus', 'active', 'disabled'];
-
-const PROPERTY_TO_TYPE: Partial<Record<keyof CSS.PropertiesHyphen, PropertyDef>> = {
-  color: { themeKey: 'colors', initialValue: '' },
-  'background-color': { themeKey: 'colors', initialValue: '' },
-  padding: { themeKey: 'space', initialValue: '0' },
-  'padding-top': { themeKey: 'space', initialValue: '0' },
-  'padding-left': { themeKey: 'space', initialValue: '0' },
-  'padding-right': { themeKey: 'space', initialValue: '0' },
-  'padding-bottom': { themeKey: 'space', initialValue: '0' },
-  margin: { themeKey: 'space', initialValue: '0' },
-  'margin-top': { themeKey: 'space', initialValue: '0' },
-  'margin-bottom': { themeKey: 'space', initialValue: '0' },
-  'margin-left': { themeKey: 'space', initialValue: '0' },
-  'margin-right': { themeKey: 'space', initialValue: '0' },
-  'column-gap': { themeKey: 'space', initialValue: '0' },
-  'row-gap': { themeKey: 'space', initialValue: '0' },
-  'border-radius': { themeKey: 'radii', initialValue: '0' },
-  width: { themeKey: 'unknown', initialValue: '' },
-  'min-width': { themeKey: 'unknown', initialValue: '' },
-  height: { themeKey: 'unknown', initialValue: '' },
-  'min-height': { themeKey: 'unknown', initialValue: '' },
-  display: { themeKey: 'unknown', initialValue: '' },
-  'flex-direction': { themeKey: 'unknown', initialValue: '' },
-  'grid-template-columns': { themeKey: 'unknown', initialValue: '' },
-  'grid-template-areas': { themeKey: 'unknown', initialValue: '' },
-  'grid-template-rows': { themeKey: 'unknown', initialValue: '' },
-  'font-size': { themeKey: 'unknown', initialValue: '' },
-  'font-weight': { themeKey: 'unknown', initialValue: '' },
-  'line-height': { themeKey: 'unknown', initialValue: '' },
-  border: { themeKey: 'unknown', initialValue: '' },
-  'border-width': { themeKey: 'unknown', initialValue: '' },
-  'border-style': { themeKey: 'unknown', initialValue: '' },
-  'text-align': { themeKey: 'unknown', initialValue: '' },
-  'object-fit': { themeKey: 'unknown', initialValue: '' },
-  overflow: { themeKey: 'unknown', initialValue: '' },
-};
 
 /* ---------------------------------------------------------------------------------------------- */
 
-export { PROPERTY_TO_TYPE, ALL_PSEUDO };
+export type { TokenamiProperty, Theme };
+export { PROPERTY_TO_TYPE, ALL_PROPERTIES, ALL_PSEUDO };
