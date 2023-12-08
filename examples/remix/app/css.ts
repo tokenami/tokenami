943049122696fabe4bab030c994abe6ba8eea4c5
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2023-12-08 20:19:21 +0000

    feat: add `VariantProps` type

--- examples/remix/app/css.ts
+++ examples/remix/app/css.ts
@@ -1,6 +1,7 @@
-import { createCss } from '@tokenami/css';
+import { type VariantProps, createCss } from '@tokenami/css';
 import config from '../.tokenami/tokenami.config.cjs';
 
 const css = createCss(config);
 
+export type { VariantProps };
 export { css };
