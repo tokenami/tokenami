923c587ae431a12645a4cb0e0292b9404e88decf
Luke Bennett <hello@lukebennett.com.au>
2024-04-07 13:53:44 +1000

    Fix Variants type

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -4,7 +4,7 @@ export type TokenamiStyle<P> = {
   [K in keyof P]: K extends 'style' ? TokenamiCSS & P[K] : P[K];
 };
 
-export type Variants<T extends () => {}> = Parameters<T>[0] extends undefined | null
+export type Variants<T extends (...args: any) => any> = Parameters<T>[0] extends undefined | null
   ? {}
   : NonNullable<Parameters<T>[0]>;
 
