ce82ca92189bbab0e6023d1273df8e575232925f
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2024-05-27 23:08:33 +0100

    monorepo improvements

--- packages/css/tsup.config.ts
+++ packages/css/tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsup';
+import { exec } from 'child_process';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
-  shims: true,
+  splitting: false,
+  treeshake: true,
+  clean: true,
+  onSuccess: async () => {
+    exec('tsc ./src/index.ts --outDir dist --emitDeclarationOnly --declaration --declarationMap');
+  },
 });
