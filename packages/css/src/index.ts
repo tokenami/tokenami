19774f23f195bd97396bd428966b2f8a729bf53c
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2025-10-03 13:29:03 +0100

    fix autocompletions for overrides

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,8 +1,8 @@
 import { type TokenamiProperties } from 'tokenami';
-import { type TokenamiCSS, type TokenamiComposeResult, type Variants as CSSVariants } from './css';
+import { type TokenamiCSS, type TokenamiComposeOutput, type Variants as CSSVariants } from './css';
 
 export type TokenamiStyle<P> = P & { style?: TokenamiProperties | TokenamiCSS };
-export type Variants<T> = T extends TokenamiComposeResult<infer V> ? CSSVariants<V> : {};
+export type Variants<T> = T extends TokenamiComposeOutput<infer V> ? CSSVariants<V> : {};
 
 export type { TokenamiProperties, TokenValue } from 'tokenami';
 export { type Config, createConfig } from '@tokenami/config';
