e82f6edd02a84ba8db665888176140cfb0e7dc45
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2026-01-25 21:00:03 +0000

    ensure cjs module interop for ts plugin (#484)

--- packages/tokenami/src/index.ts
+++ packages/tokenami/src/index.ts
@@ -1,2 +1,17 @@
 export type * from './declarations';
-export { createTSPlugin as default } from './ts-plugin';
+
+import { createTSPlugin } from './ts-plugin';
+
+interface TypeScriptPluginModule {
+  typescript: typeof import('typescript');
+}
+
+function tokenami(mod: TypeScriptPluginModule) {
+  if (mod && typeof mod === 'object' && 'typescript' in mod) {
+    return createTSPlugin(mod);
+  }
+
+  throw new Error('Tokenami only supports importing types from the root module.');
+}
+
+export default tokenami;
